6ff2edd097696afdec3bdaef7ae46eac63d9ce06
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-07-26 17:24:40 +0300

    todo logic done

--- src/components/To-do-Widget/To-do-widget.jsx
+++ src/components/To-do-Widget/To-do-widget.jsx
@@ -1,46 +1,89 @@
+import { useState } from 'react';
 import './to-do_widget.css';
 
 export default function ToDoWidget() {
+  const [inProgressItems, setInProgressItems] = useState([]);
+  const [finishedItems, setFinishedItems] = useState([]);
+  const [newItemText, setNewItemText] = useState('');
+
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    if (newItemText.trim() === '') {
+      return;
+    }
+    setInProgressItems([...inProgressItems, { status: false, task: newItemText }]);
+    setNewItemText('');
+  };
+
+  function handleDoneCheck(finishedItem) {
+    console.log(finishedItem);
+    setInProgressItems(inProgressItems.filter((item) => item !== finishedItem));
+    setFinishedItems([...finishedItems, { status: true, task: finishedItem.task }]);
+  }
+
+  function handleUnDoneCheck(itemToDo) {
+    console.log(itemToDo);
+    setFinishedItems(finishedItems.filter((item) => item !== itemToDo));
+    setInProgressItems([...inProgressItems, { status: false, task: itemToDo.task }]);
+  }
+
+  function handleTaskDelete(itemToDelete) {
+    console.log(itemToDelete);
+    if (itemToDelete.status) {
+      setFinishedItems(finishedItems.filter((item) => item !== itemToDelete));
+    }
+    setInProgressItems(inProgressItems.filter((item) => item !== itemToDelete));
+  }
+
+  function handleTasksRemove() {
+    setInProgressItems([]);
+    setFinishedItems([]);
+  }
+
   return (
     <div className='todo-widget'>
       <div className='todo_inprogress'>
-        <h3>In progress</h3>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo-discription'>Помыть посуду</p>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo-discription'>Помыть посуду</p>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo-discription'>Помыть посуду</p>
-        </div>
+        {inProgressItems.length > 0 ? <h3>In progress</h3> : null}
+        {inProgressItems.map((item) => (
+          <div className='todo_item' key={item.task}>
+            <input
+              type='checkbox'
+              checked={item.status}
+              onChange={() => handleDoneCheck(item)}
+              className='todo_checkbox'></input>
+            <p className='todo-discription'>{item.task}</p>
+            <button onClick={() => handleTaskDelete(item)}>Delete</button>
+          </div>
+        ))}
       </div>
       <div className='todo_finished'>
-        <h3>Finished</h3>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo_discription'>Пропылесосить</p>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo_discription'>Пропылесосить</p>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo_discription'>Пропылесосить</p>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' className='todo_checkbox'></input>
-          <p className='todo_discription'>Пропылесосить</p>
-        </div>
+        {finishedItems.length > 0 ? <h3>Finished</h3> : null}
+        {finishedItems.map((item) => (
+          <div className='todo_item' key={item.task}>
+            <input
+              type='checkbox'
+              checked={item.status}
+              onChange={() => handleUnDoneCheck(item)}
+              className='todo_checkbox'></input>
+            <p className='todo-discription'>{item.task}</p>
+            <button onClick={() => handleTaskDelete(item)}>Delete</button>
+          </div>
+        ))}
       </div>
-      <div className='todo_finished'>
-        <h3>Add</h3>
-        <input type='text' className='todo_checkbox'></input>
+      <div className='todo_add-bar'>
+        <h3>Add task</h3>
+        <form onSubmit={handleFormSubmit}>
+          <textarea
+            type='text'
+            value={newItemText}
+            onChange={(evt) => setNewItemText(evt.target.value)}
+            className='todo_checkbox'></textarea>
+          <button type='submit'>Add</button>
+        </form>
       </div>
+      {inProgressItems.length > 0 || finishedItems.length > 0 ? (
+        <button onClick={handleTasksRemove}>Remove all tasks</button>
+      ) : null}
     </div>
   );
 }
