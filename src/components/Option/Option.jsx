0e5eeb53a07bcfd40e6e117a8d4c32539adfdf3f
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-11-25 19:55:11 +0100

    styles in configute page refactored

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -2,6 +2,7 @@ import CalculatorWidget from '../Calculator-widget/Calculator-widget';
 import CalendarWidget from '../Calendar-widget/Calendar-widget';
 import ToDoWidget from '../To-do-widget/To-do-widget';
 import WatchWidget from '../Watch-widget/Watch-widget';
+import './option.css';
 
 export default function Option({ widget, handleWidgetCheck, isWidgetChecked }) {
   const widgets = {
@@ -17,13 +18,13 @@ export default function Option({ widget, handleWidgetCheck, isWidgetChecked }) {
 
   return (
     <>
-      <li className='configure_gridcell'>
-        <div className='configure_gridcell-bar'>
-          <h3 className='configure_gridcell-title'>{widget}</h3>
+      <li className='option'>
+        <div className='option__bar'>
+          <h3 className='option__title'>{widget}</h3>
           <input
             type='checkbox'
             name={widget}
-            className='configure_gridcell-checkbox'
+            className='option__checkbox'
             onChange={(evt) => handleWidgetCheck(evt)}
             checked={isWidgetChecked(widget)}></input>
         </div>
